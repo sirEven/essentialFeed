25aa7f2a219cdd97d0d6372ed11b68c22d8169f1
Severin Bretscher <everin@me.com>
2019-04-14 19:11:33 +0200

    Create FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,19 @@
+
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Severin Bretscher on 14.04.19.
+//  Copyright © 2019 SevDev. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+
+    
+}
