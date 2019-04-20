a60a7f476693dae96befad1aa696bdf9d69b283a
Severin Bretscher <everin@me.com>
2019-04-20 15:24:44 +0200

    give a URL instead of hardcoding it in the RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,12 +10,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
-    init(client: HTTPClient) {
+    let url: URL
+
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load(){
-       client.get(from: URL(string:  "https://a-url.com")!)
+       client.get(from: url)
     }
 }
 
@@ -31,19 +34,22 @@ class HTTPClientSpy: HTTPClient {
 }
 
 class RemoteFeedloaderTests: XCTestCase {
+
     func test_init_doesNotRequestDateFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-url.com")!
+        _ = RemoteFeedLoader(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
 
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
