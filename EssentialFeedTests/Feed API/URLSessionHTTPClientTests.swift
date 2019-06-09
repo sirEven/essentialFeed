dadf280de10b7dff3454c24d97b4334bc35384e4
Severin Bretscher <everin@me.com>
2019-06-09 20:51:37 +0200

    Guarantees correct URL and HTTP method on GET requests

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -26,10 +26,26 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
+    func test_getFromURL_performsGETRequestWithURL() {
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "http://any-url.com")!
+        let exp = expectation(description: "Wait for request")
 
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+
+        URLSessionHTTPClient().get(from: url) { _ in }
+
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequest()
+    }
+    
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
-        let url = URL(string: "http://any-url.com")
+        let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "anny error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
 
@@ -37,7 +53,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
         let exp = expectation(description: "Wait for completion")
 
-        sut.get(from: url!) { result in // <- trailing closure to handle completion block with its value
+        sut.get(from: url) { result in // <- trailing closure to handle completion block with its value
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
@@ -46,6 +62,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             }
             exp.fulfill()
         }
+
         wait(for: [exp], timeout: 1.0)
         URLProtocolStub.stopInterceptingRequest()
     }
@@ -57,6 +74,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     */
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
+        private static var requestObserver: ((URLRequest) -> Void)?
 
         private struct Stub {
             let data: Data?
@@ -68,6 +86,10 @@ class URLSessionHTTPClientTests: XCTestCase {
             stub = Stub(data: data, response: response, error: error)
         }
 
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+
         static func startInterceptingRequests() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
@@ -75,6 +97,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptingRequest() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
+            requestObserver = nil
         }
 
         /*
@@ -83,6 +106,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         Request (meaning, if we have a Stub for the given url) <3
         */
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
 
