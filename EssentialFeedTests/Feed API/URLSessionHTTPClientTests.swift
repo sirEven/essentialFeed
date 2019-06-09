aa4a1843c4178086c86b10c908632d9ac3c268ac
Severin Bretscher <everin@me.com>
2019-06-09 23:17:28 +0200

    Extract duplicate code into a new shared reusable helper method to simplify tests

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -28,7 +28,7 @@ class URLSessionHTTPClient {
             } else {
                 completion(.failure(UnexpectedValuesRepresentation()))
             }
-        }.resume()
+            }.resume()
     }
 }
 
@@ -42,7 +42,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     override func tearDown() {
         super.tearDown()
-         URLProtocolStub.stopInterceptingRequest()
+        URLProtocolStub.stopInterceptingRequest()
     }
 
     func test_getFromURL_performsGETRequestWithURL() {
@@ -83,46 +83,23 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_succedsOnHTTPURLResponseWithData() {
         let data = anyData()
         let response = anyHTTPURLResponse()
-        URLProtocolStub.stub(data: data, response: response, error: nil)
 
-        let exp = expectation(description: "Wait for completion")
-        makeSUT().get(from: anyURL()) { result in
-            switch result {
-            case let .success(receivedData, receivedResponse):
-                XCTAssertEqual(receivedData, data)
-                XCTAssertEqual(receivedResponse.url, response.url)
-                XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
-            default:
-                XCTFail("Expected success, got \(result) instead")
-            }
+        let receivedValues = resultValuesFor(data: data, response: response, error: nil)
 
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(receivedValues?.data, data)
+        XCTAssertEqual(receivedValues?.response.url, response.url)
+        XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
     }
 
     func test_getFromURL_succedsWithEmptyDataOnHTTPURLResponseWithNilData() {
         let response = anyHTTPURLResponse()
-        URLProtocolStub.stub(data: nil, response: response, error: nil)
+        let receivedValues = resultValuesFor(data: nil, response: response, error: nil)
 
-        let exp = expectation(description: "Wait for completion")
-        makeSUT().get(from: anyURL()) { result in
-            switch result {
-            case let .success(receivedData, receivedResponse):
-                let emptyData = Data()
-                XCTAssertEqual(receivedData, emptyData)
-                XCTAssertEqual(receivedResponse.url, response.url)
-                XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
-            default:
-                XCTFail("Expected success, got \(result) instead")
-            }
-
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        let emptyData = Data()
+        XCTAssertEqual(receivedValues?.data, emptyData)
+        XCTAssertEqual(receivedValues?.response.url, response.url)
+        XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
     }
-
-    
 
     // MARK: - Helpers
 
@@ -132,25 +109,44 @@ class URLSessionHTTPClientTests: XCTestCase {
         return sut
     }
 
+    private func resultValuesFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> (data: Data, response: HTTPURLResponse)? {
+        let result = resultFor(data: data, response: response, error: error, file: file, line: line)
+
+        switch result {
+        case let .success(data, response):
+            return (data, response)
+        default:
+            XCTFail("Expected success, got \(result) instead", file: file, line: line)
+            return nil
+        }
+    }
+
     private func resultErrorFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> Error? {
+        let result = resultFor(data: data, response: response, error: error, file: file, line: line)
+
+        switch result {
+        case let .failure(error):
+            return error
+        default:
+            XCTFail("Expected failure, got \(result) instead", file: file, line: line)
+            return nil
+        }
+    }
+
+    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> HTTPClientResult {
         URLProtocolStub.stub(data: data, response: response, error: error)
         let sut = makeSUT(file: file, line: line)
         let exp = expectation(description: "Wait for completion")
 
-        var receivedError: Error?
+        var receivedResult: HTTPClientResult!
         sut.get(from: anyURL()) { result in // <- trailing closure to handle completion block with its value
-            switch result {
-            case let .failure(error):
-                receivedError = error
-            default:
-                XCTFail("Expected failure, got \(result) instead", file: file, line: line)
-            }
+            receivedResult = result
 
             exp.fulfill()
         }
 
         wait(for: [exp], timeout: 1.0)
-        return receivedError
+        return receivedResult
     }
 
     private func anyURL() -> URL{
@@ -175,7 +171,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     /*
      Subclassing, an abstract class (which happens to be named "Protocol")
-    */
+     */
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
@@ -205,10 +201,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         /*
-        canInit is a static class method  -> this means, we don't have an instance yet at this point.
-        The url loading system will only instantiate our URLProtocolStub, if we can handle the
-        Request (meaning, if we have a Stub for the given url) <3
-        */
+         canInit is a static class method  -> this means, we don't have an instance yet at this point.
+         The url loading system will only instantiate our URLProtocolStub, if we can handle the
+         Request (meaning, if we have a Stub for the given url) <3
+         */
         override class func canInit(with request: URLRequest) -> Bool {
             requestObserver?(request)
             return true
@@ -219,8 +215,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         /*
-        startLoading is an instance method now, so the framework has accepted that we're going to
-        handle this request. "now it's time to start loading the url".
+         startLoading is an instance method now, so the framework has accepted that we're going to
+         handle this request. "now it's time to start loading the url".
          */
         override func startLoading() {
             if let data = URLProtocolStub.stub?.data {
