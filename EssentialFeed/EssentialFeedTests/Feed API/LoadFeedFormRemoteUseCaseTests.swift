4590d2fcc79198d0e0e9047c198ce82ac6603f1d
Severin Bretscher <sir_even@icloud.com>
2019-10-27 12:33:08 +0100

    Replace custom 'HTTPClientResult' enum with a nested typealias over the standard 'Swift.Result'

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFormRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFormRemoteUseCaseTests.swift
@@ -141,7 +141,7 @@ class LoadFeedFormRemoteUseCaseTests: XCTestCase {
             "location": location,
             "image": imageURL.absoluteString
             ].compactMapValues { $0 }
-        
+
         return (item, json)
     }
 
@@ -174,13 +174,13 @@ class LoadFeedFormRemoteUseCaseTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
 
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
 
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
 
@@ -195,7 +195,7 @@ class LoadFeedFormRemoteUseCaseTests: XCTestCase {
                 httpVersion: nil,
                 headerFields: nil
             )!
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -110,12 +110,12 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
     }
 
-    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> HTTPClientResult {
+    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> HTTPClient.Result {
         URLProtocolStub.stub(data: data, response: response, error: error)
         let sut = makeSUT(file: file, line: line)
         let exp = expectation(description: "Wait for completion")
 
-        var receivedResult: HTTPClientResult!
+        var receivedResult: HTTPClient.Result!
         sut.get(from: anyURL()) { result in // <- trailing closure to handle completion block with its value
             receivedResult = result
 
