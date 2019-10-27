faf1b765babbdd218bdec9f4a3062562766b7973
Severin Bretscher <sir_even@icloud.com>
2019-10-27 12:50:39 +0100

    Replace custom 'RetrieveCachedFeedResult' enum with a nested typealias over the standard 'Swift.Result' (FeedStore.RetrievalResult)

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -4,16 +4,17 @@
 
 import Foundation
 
-public enum RetrievedCachedFeedResult {
+public enum CachedFeed {
     case empty
     case found(feed: [LocalFeedImage], timestamp: Date)
-    case failure(Error)
 }
 
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetrievedCachedFeedResult) -> Void
+
+    typealias RetrievalResult = Result<CachedFeed, Error>
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
 
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
