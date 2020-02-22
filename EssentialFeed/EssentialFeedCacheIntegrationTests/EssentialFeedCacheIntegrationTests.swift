445235eeb8c1dab38890de49181a7a0cb88b0c40
Severin Bretscher <sir_even@icloud.com>
2020-02-22 19:01:36 +0100

    'LocalFeedLoader' in integration with the 'CoreDataFeedStore' does not delete recently saved items saved by separate instances when validating cache, proving we correctly manage the data models on disk

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -49,6 +49,17 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         expect(feedLoaderToPerformLoad, toLoad: latestFeed)
     }
 
+    func test_validateFeedCache_doesNotDeleteRecentlySavedFeed() {
+         let feedLoaderToPerformSave = makeFeedLoader()
+         let feedLoaderToPerformValidation = makeFeedLoader()
+         let feed = uniqueImageFeed().models
+
+         save(feed, with: feedLoaderToPerformSave)
+         validateCache(with: feedLoaderToPerformValidation)
+
+         expect(feedLoaderToPerformSave, toLoad: feed)
+     }
+
     // MARK: - LocalFeedImageDataLoader Tests
 
     func test_loadImageData_deliversSavedDataOnASeparateInstance() {
@@ -79,7 +90,7 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 
          expect(imageLoaderToPerformLoad, toLoad: lastImageData, for: image.url)
      }
-    
+
     // MARK: - Helpers
 
     private func makeFeedLoader(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
@@ -110,6 +121,17 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [saveExp], timeout: 1.0)
     }
+
+    private func validateCache(with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+         let saveExp = expectation(description: "Wait for save completion")
+         loader.validateCache() { result in
+             if case let Result.failure(error) = result {
+                 XCTFail("Expected to validate feed successfully, got error: \(error)", file: file, line: line)
+             }
+             saveExp.fulfill()
+         }
+         wait(for: [saveExp], timeout: 1.0)
+     }
 
     private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
