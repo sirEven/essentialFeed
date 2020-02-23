b3dbc7c77ce3447574b4bf9832d78f12fae29651
Severin Bretscher <sir_even@icloud.com>
2020-02-23 16:34:17 +0100

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2020 SevDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
