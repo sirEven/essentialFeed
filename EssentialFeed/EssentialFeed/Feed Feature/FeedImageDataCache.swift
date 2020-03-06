ba577a5e1efa9e6a143f337b57834c6ee7f7a965
Severin Bretscher <sir_even@icloud.com>
2020-03-06 18:20:54 +0100

    Move 'FeedImageDataCache' to the 'Feed Feature' production group

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2020 SevDev. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
