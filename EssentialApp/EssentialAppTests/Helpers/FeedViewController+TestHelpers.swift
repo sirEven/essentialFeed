11e1d2c66d38201de397a8f944d3fc24850b8439
Severin Bretscher <sir_even@icloud.com>
2020-03-18 19:10:42 +0100

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -40,6 +40,10 @@ extension FeedViewController {
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
 
+    func renderedFeedImagegData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
+
     var errorMessage: String? {
         return errorView?.message
     }
